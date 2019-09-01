b7d9acbd4c63264fc8cae33fcb7b5885c2119a7a
code <code@qq.com>
2019-09-01 14:31:19 +0800

    维护代码

--- waimai-client/vue.config.js
+++ waimai-client/vue.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  baseUrl: '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   indexPath: 'index.html',
   filenameHashing: true,
   lintOnSave: true, // 若为false则代码不符合elint的规则会编译失败
   devServer: {
-      open: true,
-      port: 8081,
+      open: false,
+      port: 8888,
       proxy: {
         '/api': {
           target: 'http://localhost:4000',
